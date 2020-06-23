4d005aabb84534cffec6322541927c32088e4595
Aditya Paul <adityapaul@my.unt.edu>
2020-06-23 10:32:41 -0500

    Okay last quick cleanup

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,5 +1,5 @@
 import React, { useContext } from "react";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 import FirebaseContext from "../firebase/firebaseContext";
 import AuthContext from "../auth/authContext";
 import "./Home.css";
